c64adc897e9e4662cdd35fe7858b1f22bb364ea4
JPol0 <jeanpaulcora@gmail.com>
2025-01-11 21:44:03 -0400

    Update LoadingScreen.java

--- core/src/main/java/com.marbro/screens/LoadingScreen.java
+++ core/src/main/java/com.marbro/screens/LoadingScreen.java
@@ -3,8 +3,6 @@ package com.marbro.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
