0901d7bdb7d62fd381676b35ecb32d39e77c7735
JPol0 <jeanpaulcora@gmail.com>
2025-01-14 17:38:45 -0400

    modificacion Factory Kirby

--- core/src/main/java/com.marbro/entities/player/FactoryKirby.java
+++ core/src/main/java/com.marbro/entities/player/FactoryKirby.java
@@ -2,6 +2,10 @@ package com.marbro.entities.player;
 
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.marbro.entities.enemies.Factory.Enemy;
+import com.marbro.entities.enemies.Sir_Kibble.Sir_Kibble;
+import com.marbro.entities.enemies.waddle_dee.Waddle_dee;
+import com.marbro.entities.player.kirby_base.ColisionesHandlerKirby;
 import com.marbro.entities.player.kirby_base.Kirby_base;
 import com.marbro.entities.player.kirby_parasol.Kirby_Parasol;
 import com.marbro.screens.level1.Level1;
@@ -9,16 +13,32 @@ import com.marbro.screens.level1.Level1;
 import static com.marbro.constants.Constantes.PPM;
 
 public class FactoryKirby {
-    public Kirby createKirby(Level1 gameScreen, Body body, Rectangle rectangle, String type_enemy, int salud) {
-        switch (type_enemy) {
-            case "Waddle_dee":
-                return new Kirby_Parasol(gameScreen.getWorld(), gameScreen.getStage(), body,
-                    gameScreen.getControlador(), rectangle.width / PPM, rectangle.height / PPM,
-                    gameScreen, salud);
-            default:
-                return new Kirby_base(gameScreen.getWorld(),
-                    gameScreen.getStage(), body, gameScreen.getControlador(),
-                    rectangle.width / PPM, rectangle.height / PPM, gameScreen, salud);
+    public Kirby createKirby(Level1 gameScreen, Body body, Rectangle rectangle, Enemy type_enemy, int salud, ColisionesHandlerKirby colisionesHandlerKirby) {
+
+        if(type_enemy == null)
+        {
+            return new Kirby_base(gameScreen.getWorld(),
+                gameScreen.getStage(), body, gameScreen.getControlador(),
+                rectangle.width / PPM, rectangle.height / PPM, gameScreen, salud, colisionesHandlerKirby);
+        }
+
+        if(type_enemy instanceof Waddle_dee)
+        {
+            return new Kirby_Parasol(gameScreen.getWorld(), gameScreen.getStage(), body,
+                gameScreen.getControlador(), rectangle.width / PPM, rectangle.height / PPM,
+                gameScreen, salud, colisionesHandlerKirby);
+        }
+        else if (type_enemy instanceof Sir_Kibble)
+        {
+            return new Kirby_Parasol(gameScreen.getWorld(), gameScreen.getStage(), body,
+                gameScreen.getControlador(), rectangle.width / PPM, rectangle.height / PPM,
+                gameScreen, salud, colisionesHandlerKirby);
+        }
+        else
+        {
+            return new Kirby_base(gameScreen.getWorld(),
+                gameScreen.getStage(), body, gameScreen.getControlador(),
+                rectangle.width / PPM, rectangle.height / PPM, gameScreen, salud, colisionesHandlerKirby);
 
         }
     }
